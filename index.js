3feb63b82b5d53f8ab6ab1d3bc81b6b68fb6d3b9
Sapphire-Software <sapphire.greengene@gmail.com>
2015-05-09 17:55:12 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#staff").mouseover(function(){
+        $("#staff").css("color", "#FFFF66");
+    });
+	$("#staff").mouseout(function(){
+		$("#staff").css("color", "#ECECEC");
+    });
+});
